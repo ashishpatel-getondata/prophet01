08d487860935cf2a50e1e77b4af16483fa60fc50
Ashish Patel <159769595+ashishpatel-getondata@users.noreply.github.com>
2025-06-08 14:42:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,31 @@
-#https://davidmarquis.hashnode.dev/create-a-basic-forecasting-app-using-streamlit-and-prophet
+# https://davidmarquis.hashnode.dev/create-a-basic-forecasting-app-using-streamlit-and-prophet
 import streamlit as st
 import pandas as pd
 from prophet import Prophet
-from prophet.plot import plot_plotly, plot_components_plotly
+from prophet.plot import plot_plotly
 
-#Load prophet example data into a pandas dataframe
-df = pd.read_csv('https://raw.githubusercontent.com/facebook/prophet/main/examples/example_wp_log_peyton_manning.csv')
+def main():
+    # Set Streamlit app title and description
+    st.title('Time Series Forecasting App')
+    st.write('My Forecast Data.')
 
-def forecast(df):
-    # Create a Prophet model
+    # Load prophet example data into a pandas dataframe
+    df = pd.read_csv('https://raw.githubusercontent.com/facebook/prophet/main/examples/example_wp_log_peyton_manning.csv')
+
+    # Create and fit the Prophet model
     m = Prophet()
-
-    # Fit the model to the data
     m.fit(df)
 
     # Make future predictions
     future = m.make_future_dataframe(periods=365)
     forecast = m.predict(future)
 
-    # Forecast plot
-    plot_plotly(m, forecast)
+    # Display the forecast output
+    st.dataframe(forecast)
 
-    #Return the forecast DataFrame
-    return forecast
+    # Display forecast plot
+    fig_forecast = st.plotly_chart(plot_plotly(m, forecast))
+    st.plotly_chart(fig_forecast)
 
-
-def main():
-  # Set Streamlit app title and description
-  st.title('Time Series Forecasting App')
-  st.write('My Forecast Data.')
-
-  forecast_data = forecast(df)
-
-  # Display the forecast output
-  st.dataframe(forecast_data)
-
- 
-
-  
 if __name__ == '__main__':
-   main()
+    main()
