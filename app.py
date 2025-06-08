85f220e569e6efa9bf84255b5b628f52ac266ddc
Ashish Patel <159769595+ashishpatel-getondata@users.noreply.github.com>
2025-06-08 14:07:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,35 @@
 import streamlit as st
-from datetime import date
-
-import yfinance as yf
+import pandas as pd
 from prophet import Prophet
-from prophet.plot import plot_plotly
-from plotly import graph_objs as go 
 
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title("Stock Prediction App")
+#Load prophet example data into a pandas dataframe
+df = pd.read_csv('https://raw.githubusercontent.com/facebook/prophet/main/examples/example_wp_log_peyton_manning.csv')
 
-# APPL - Apple, GOOG - Google, MSFT - Microsoft, GME - Gamestop
-stocks = ("AAPL", "GOOG", "MSFT", "GME")
+def forecast(df):
+    # Create a Prophet model
+    m = Prophet()
 
-selected_stock = st.selectbox("Select dataset for prediction", stocks)
+    # Fit the model to the data
+    m.fit(df)
 
-n_years = st.slider("Years of Prediction:", 1, 4)
-periods = n_years * 365
+    # Make future predictions
+    future = m.make_future_dataframe(periods=365)
+    forecast = m.predict(future)
 
-def load_data(ticker):
-    data = yf.download(ticker.strip(), START, TODAY)
-    data = data.reset_index(inplace = True)
-    return data
+    #Return the forecast DataFrame
+    return forecast
 
-data_load_state = st.text("Loading data...")
-data = load_data(selected_stock)
-data_load_state.text("Loading data done.")
 
-st.subheader("Raw Data")
-st.write(data.tail())
+def main():
+  # Set Streamlit app title and description
+  st.title('Time Series Forecasting App')
+  st.write('My Forecast Data.')
 
-def plot_raw_data(data):
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
-    fig.update_layout(title_text="Time Series Data", xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
+  forecast_data = forecast(df)
 
-plot_raw_data()
+  # Display the forecast output
+  st.dataframe(forecast_data)
+  
+if __name__ == '__main__':
+   main()
